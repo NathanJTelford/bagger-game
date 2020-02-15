a85bd24dec27df236f9d4059fb7861b374418c33
NathanJTelford <nathan.j.i.telford@gmail.com>
2020-02-14 22:03:26 -0700

    made css edits

--- src/components/landing/Modal.js
+++ src/components/landing/Modal.js
@@ -4,7 +4,7 @@ import './Landing.scss';
 
 const modal = (props)=>{
     return(
-        <div className='opening-modal'><button onClick={()=>props.firstDialog('start')}>Begin</button></div>
+        <div className='opening-modal'><button className='opening-modal-button' onClick={()=>props.firstDialog('start')}>Begin</button></div>
     )
 }
 export default modal
